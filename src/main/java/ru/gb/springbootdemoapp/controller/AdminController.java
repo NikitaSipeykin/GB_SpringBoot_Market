48e501219903e3770e20426a109e407297a0343b
MacNikSipeykin <niksipeykin@gmail.com>
2025-06-16 10:10:46 +0300

    Add files upload fetch

--- src/main/java/ru/gb/springbootdemoapp/controller/AdminController.java
+++ src/main/java/ru/gb/springbootdemoapp/controller/AdminController.java
@@ -4,16 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.webjars.NotFoundException;
 import ru.gb.springbootdemoapp.converter.ProductMapper;
 import ru.gb.springbootdemoapp.dto.ProductDto;
 import ru.gb.springbootdemoapp.dto.ProductShortDto;
 import ru.gb.springbootdemoapp.service.CategoryService;
 import ru.gb.springbootdemoapp.service.ProductService;
+import ru.gb.springbootdemoapp.service.StorageService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -26,11 +25,13 @@ public class AdminController {
   private final ProductService productService;
   private final CategoryService categoryService;
   private final ProductMapper productMapper;
+  private final StorageService storageService;
 
-  public AdminController(ProductService productService, CategoryService categoryService, ProductMapper productMapper) {
+  public AdminController(ProductService productService, CategoryService categoryService, ProductMapper productMapper, StorageService storageService) {
     this.productService = productService;
     this.categoryService = categoryService;
     this.productMapper = productMapper;
+    this.storageService = storageService;
   }
 
   @GetMapping
@@ -51,11 +52,14 @@ public class AdminController {
 
   @PostMapping("/add")
   @Transactional
-  public String saveStudent(@Valid ProductShortDto productShortDto, BindingResult bindingResult, Model model) {
+  public String saveStudent(@Valid ProductShortDto productShortDto, @RequestParam MultipartFile image,
+                            BindingResult bindingResult, Model model) {
     if (bindingResult.hasErrors()) {
       return "admin/add_product_form";
     }
     try {
+      storageService.store(image);
+      productShortDto.setImageUrl("/media/" + image.getOriginalFilename());
       productService.save(productMapper.productShortDtoToProduct(productShortDto));
     } catch (NotFoundException ex) {
       model.addAttribute("notFound", ex);
